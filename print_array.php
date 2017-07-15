8dc034e5e9704849654f479803016ac2634e9cdb
Nguyễn Hải Quang <haiquang9994@gmail.com>
2017-07-15 10:01:57 +0700

    Update print_array.php

--- print_array.php
+++ print_array.php
@@ -7,12 +7,12 @@ function print_array(array $array = [], $level = 1, $open = true)
         $content .= str_repeat(' ', 4 * ($level - 1))."[\n";
     }
     foreach ($array as $key => $value) {
-        $content .= str_repeat(' ', 4 * $level).(preg_match('/^[0-9]+$/', $key) ? $key : "'$key'").' => ';
+        $content .= str_repeat(' ', 4 * $level).(preg_match('/^[1-9][0-9]*$/', $key) ? $key : "'$key'").' => ';
         if (is_array($value)) {
             $content .= "[\n";
-            $content .= print_array($value, $level + 1, false);
+            $content .= $this->print_array($value, $level + 1, false);
         } else {
-            $content .= preg_match('/^[0-9]+$/', $value) ? $value : "'$value'";
+            $content .= preg_match('/^[1-9][0-9]*$/', $value) ? $value : "'$value'";
             $content .= ",\n";
         }
     }
