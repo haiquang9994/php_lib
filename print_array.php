8ef310f49f01adad6d72c0ffa3e4786fa45b71df
Nguyễn Hải Quang <haiquang9994@gmail.com>
2019-12-26 11:44:24 +0700

    Update print_array.php

--- print_array.php
+++ print_array.php
@@ -1,6 +1,21 @@
 <?php
 
-function print_array(array $array = [], $level = 1, $open = true) {
+function print_value($value)
+{
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+    if (is_numeric($value)) {
+        return $value;
+    }
+    if (is_object($value)) {
+        $value = json_encode($value);
+    }
+    return "'" . addslashes($value) . "'";
+}
+
+function print_array(array $array = [], $level = 1, $open = true)
+{
     $content = "";
     if ($open) {
         $content .= str_repeat(' ', 4 * ($level - 1)) . "[\n";
@@ -11,7 +26,7 @@ function print_array(array $array = [], $level = 1, $open = true) {
             $content .= "[\n";
             $content .= print_array($value, $level + 1, false);
         } else {
-            $content .= preg_match('/^[1-9][0-9]*$|^0$/', $value) ? $value : "'" . addslashes($value) . "'";
+            $content .= print_value($value);
             $content .= ",\n";
         }
     }
