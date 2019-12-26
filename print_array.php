4f03904b34f9cf7fb587fc70e01a2281cc7f528c
Nguyễn Hải Quang <haiquang9994@gmail.com>
2019-12-26 11:34:36 +0700

    Update print_array.php

--- print_array.php
+++ print_array.php
@@ -1,21 +1,20 @@
 <?php
 
-function print_array(array $array = [], $level = 1, $open = true)
-{
+function print_array(array $array = [], $level = 1, $open = true) {
     $content = "";
     if ($open) {
-        $content .= str_repeat(' ', 4 * ($level - 1))."[\n";
+        $content .= str_repeat(' ', 4 * ($level - 1)) . "[\n";
     }
     foreach ($array as $key => $value) {
-        $content .= str_repeat(' ', 4 * $level).(preg_match('/^[1-9][0-9]*$|^0$/', $key) ? $key : "'".addslashes($key)."'").' => ';
+        $content .= str_repeat(' ', 4 * $level) . (preg_match('/^[1-9][0-9]*$|^0$/', $key) ? $key : "'" . addslashes($key) . "'") . ' => ';
         if (is_array($value)) {
             $content .= "[\n";
             $content .= print_array($value, $level + 1, false);
         } else {
-            $content .= preg_match('/^[1-9][0-9]*$|^0$/', $value) ? $value : "'".addslashes($value)."'";
+            $content .= preg_match('/^[1-9][0-9]*$|^0$/', $value) ? $value : "'" . addslashes($value) . "'";
             $content .= ",\n";
         }
     }
-    $content .= str_repeat(' ', 4 * ($level - 1))."]".($level === 1 ? ';' : ',')."\n";
+    $content .= str_repeat(' ', 4 * ($level - 1)) . "]" . ($level === 1 ? ';' : ',') . "\n";
     return $content;
 }
